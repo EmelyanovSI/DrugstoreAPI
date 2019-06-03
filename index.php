5d60db1fd9b2cd328b607b44e75b79acc696eab1
ikon <emelyanovsergeyofficial@gmail.com>
2019-06-03 12:35:37 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$response = array();
+$response["success"] = "1";
+$response["message"] = "WELCOME";
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
